eff532c65994583b23c673a73a675dbeff026b99
fpv13 <91254221+fpv13@users.noreply.github.com>
2021-09-25 17:03:41 +0300

    1703 250921

---
+++ src/array.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class array {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int sum=0;
+        System.out.println("Enter number array");
+        int length = sc.nextInt();
+        int[] array = new int[length];
+
+        for(int i=0;i<length;i++){
+            System.out.println("Enter"+i+": ");
+            array[i]=sc.nextInt();
+            if(i%4==0){
+                sum=sum+i;
+                System.out.println(sum);
+            }
+        }
+
+        System.out.println("Summa cratno 4 is"+sum);
+    }
+}
